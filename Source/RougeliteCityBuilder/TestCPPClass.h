646484ab4fb3c727c13b6bf7adb917f0f065dfda
atomdaly <mrthomasdaly@gmail.com>
2022-04-21 17:41:40 +0100

    Added C++ files

---
+++ Source/RougeliteCityBuilder/TestCPPClass.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class ROUGELITECITYBUILDER_API TestCPPClass
+{
+public:
+	TestCPPClass();
+	~TestCPPClass();
+};
